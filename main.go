56062432598b0d11dda110d652d290f9e5ce683a
Paul Smith <paulsmith@pobox.com>
2017-08-11 14:29:56 -0500

    Update main.go

--- main.go
+++ main.go
@@ -67,6 +67,7 @@ h1 {
     <body>
         <h1>Hello, {{ .Name }}</h1>
 	<hr>
+	<p>Hello, VA</p>
 	<p>Try setting the name with a URL parameter <code>?name=FOO</code>.</p>
         <p>Examples:</p>
         <ul>

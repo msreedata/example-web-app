6ee271e41f10067f89ea712debd6fe47925b83b9
Paul Smith <paulsmith@pobox.com>
2017-10-11 15:17:14 -0500

    Update main.go

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func home(w http.ResponseWriter, r *http.Request) {
         <title>Hello, {{ .Name }}</title>
         <style>
           html, body {
-              background: purple;
+              background: green;
               color: rgba(255, 255, 255, 0.9);
               font-family: georgia, serif;
           }
@@ -67,7 +67,7 @@ h1 {
     <body>
         <h1>Hello, {{ .Name }}</h1>
 	<hr>
-	<p>Hello, Melanie, Louis, and Wyatt (and Mike too)</p>
+	<p>Hello, Devoted Health!</p>
 	<p>Try setting the name with a URL parameter <code>?name=FOO</code>.</p>
         <p>Examples:</p>
         <ul>

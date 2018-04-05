4ce56c69ae1851afadc6fc37995c644730c81367
Paul Smith <paul@adhocteam.us>
2018-04-05 13:56:43 -0500

    Update bg color, remove custom greeting

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func home(w http.ResponseWriter, r *http.Request) {
         <title>Hello, {{ .Name }}</title>
         <style>
           html, body {
-              background: green;
+              background: hsl(206, 82%, 58%);
               color: rgba(255, 255, 255, 0.9);
               font-family: georgia, serif;
           }
@@ -67,7 +67,6 @@ h1 {
     <body>
         <h1>Hello, {{ .Name }}</h1>
 	<hr>
-	<p>Hello, Devoted Health!</p>
 	<p>Try setting the name with a URL parameter <code>?name=FOO</code>.</p>
         <p>Examples:</p>
         <ul>
